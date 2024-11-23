b3744e886b7696d3b9fdb5b7c8ba732ef431148e
Rcoder-singh <rishabh22.singh@gmail.com>
2024-11-23 20:04:11 +0530

    Created BrowserRouter

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,21 @@
-function App() {
-  return <>Start here</>;
-}
+import React from "react";
+import Navbar from "./Components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./Pages/SignUp";
+import Login from "./Pages/Login";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/register" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
