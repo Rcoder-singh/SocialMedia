c6984cefa1e53853c0e7d0d979d43aa1190c277d
Rcoder-singh <rishabh22.singh@gmail.com>
2024-11-27 19:11:09 +0530

    Added CheckToken middleware

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const CheckToken = require("../Middleware/CheckToken");
 const {
-  registerUser,
+  userInfo,
   loginUser,
   updateUser,
   deleteUser,
+  registerUser,
 } = require("../Controllers/userController");
 
-router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.put("/update/:_id", updateUser);
-router.delete("/delete/:_id", deleteUser);
+router.get("/get_user_info", CheckToken, userInfo);
+router.post("/register", registerUser);
+router.delete("/delete/:_id",CheckToken, deleteUser);
+router.put("/update/:_id", CheckToken, updateUser);
 
 module.exports = router;
